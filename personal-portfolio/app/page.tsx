e6cd0a0212ed7743f8670c640a6bfdf450ad2c04
devKire <erikdossantos2006@outlook.com>
2024-11-14 22:35:46 -0300

    initial commit

--- personal-portfolio/app/page.tsx
+++ personal-portfolio/app/page.tsx
@@ -2,8 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./components/ui/button";
 
-import { Input } from "./components/ui/input";
-import { Textarea } from "./components/ui/textarea";
 import ProjectSpecs from "./components/ProjectSpecs";
 
 import SkillsSection from "./components/Skillssection";
